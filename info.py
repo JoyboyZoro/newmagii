7ad4e086b8beb6280e2f34d95e2bd0d52146de12
JoyboyZoro <154298406+JoyboyZoro@users.noreply.github.com>
2024-01-15 21:50:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -102,13 +102,13 @@ NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME')
+    APP_NAME = environ.get('APP_NAME', 'clone')
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://illegal-filter-bot-f33ec61d18ce.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://illegal-filter-bot-f33ec61d18ce.herokuapp.com/".format(FQDN, PORT)
+URL = "https://clone-d6d848f73cc0.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://clone-d6d848f73cc0.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
